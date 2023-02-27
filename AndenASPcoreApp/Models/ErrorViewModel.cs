6e74e797f8b2e4f7e6169d7d6d65973d9b18c235
Rantala95 <hardshufflestuff@gmail.com>
2023-02-27 09:46:13 +0100

    Add project files.

---
+++ AndenASPcoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AndenASPcoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
