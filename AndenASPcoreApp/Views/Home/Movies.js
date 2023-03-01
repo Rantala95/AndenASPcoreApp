a7385c2b6612a516162c21975d9fecbb6fc6dfa3
Martin Aqqaluk Rantala <hardshufflestuff@gmail.com>
2023-03-01 14:58:19 +0100

    01-03-2023

--- AndenASPcoreApp/Views/Home/Movies.js
+++ AndenASPcoreApp/Views/Home/Movies.js
@@ -45,4 +45,31 @@
 function isValidUrl(url) {
     // Check if the URL starts with http or https and ends with an image file extension
     return /^https?:\/\/.*\.(jpg|jpeg|png|gif)$/i.test(url);
+} dataType: "json",
+    success: function (data) {
+        var movies = data.Search;
+        if (isValidUrl(movies[0].Poster)) {
+            $("#movie-container1").html("<h2>" + movies[0].Title + "</h2> <img src='" + movies[0].Poster + "'>");
+        } else {
+            console.log("Error: Invalid poster URL for movie " + movies[0].Title);
 }
+        if (isValidUrl(movies[1].Poster)) {
+            $("#movie-container2").html("<h2>" + movies[1].Title + "</h2> <img src='" + movies[1].Poster + "'>");
+        } else {
+            console.log("Error: Invalid poster URL for movie " + movies[1].Title);
+        }
+        if (isValidUrl(movies[2].Poster)) {
+            $("#movie-container3").html("<h2>" + movies[2].Title + "</h2> <img src='" + movies[2].Poster + "'>");
+        } else {
+            console.log("Error: Invalid poster URL for movie " + movies[2].Title);
+        }
+        if (isValidUrl(movies[3].Poster)) {
+            $("#movie-container4").html("<h2>" + movies[3].Title + "</h2> <img src='" + movies[3].Poster + "'>");
+        } else {
+            console.log("Error: Invalid poster URL for movie " + movies[3].Title);
+        }
+    },
+error: function (jqXHR, textStatus, errorThrown) {
+    console.log("Error: " + textStatus + " " + errorThrown);
+}
+    });
